2920c425bab28f0dfa48d6fca9acf084361af6f0
Procid-GIT <Leo.Jiang@comcast.net>
2026-06-19 13:41:04 -0400

    prevented the website from redirecting user to forbidden url

--- script.js
+++ script.js
@@ -1,40 +1,42 @@
 const backend_url = "https://procid-ocr-rrecognition-backend.hf.space/api/predict"
-const btn = document.getElementById("sub");
-btn.addEventListener("click", processOCR);
 
-async function processOCR(e) {
-    if (e) e.preventDefault(); 
-    const fileInput = document.getElementById("imageUpload");
-    const output = document.getElementById("output");
+document.addEventListener("DOMContentLoaded", () => {
+    const btn = document.getElementById("sub");
+    btn.addEventListener("click", async (e) => {
+        e.preventDefault(); 
+        const fileInput = document.getElementById("imageUpload");
+        const output = document.getElementById("output");
 
-    if (fileInput.files.length === 0) {
-        alert("Please upload image first, ok?");
-        return;
-    }
-
-    const formData = new FormData();
-    formData.append("aiImage", fileInput.files[0]);
-    try {
-        btn.disabled = true;
-        output.textContent = "...";
-
-        const Response = await fetch(backend_url, {
-            method: 'POST',
-            body: formData
-        })
-
-        if (!Response.ok) {
-            throw new Error(`Sorry, server returned with status code ${Response.status}`);
+        if (fileInput.files.length === 0) {
+            alert("Please upload image first, ok?");
+            return;
         }
 
-        const result = await Response.json();
-        output.textContent = `${result.prediction}`;
+        const formData = new FormData();
+        formData.append("aiImage", fileInput.files[0]);
+        try {
+            btn.disabled = true;
+            output.textContent = "...";
+
+            const Response = await fetch(backend_url, {
+                method: 'POST',
+                body: formData
+            })
+
+            if (!Response.ok) {
+                throw new Error(`Sorry, server returned with status code ${Response.status}`);
+            }
+
+            const result = await Response.json();
+            output.textContent = `${result.prediction}`;
 
 
-    } catch (error) {
-        console.error("Connection Error: ", error);
-        output.textContent=("Sorry, unable to send, error.");
-    } finally {
-        btn.disabled = false;
-    }
-}
+        } catch (error) {
+            console.error("Connection Error: ", error);
+            output.textContent=("Sorry, unable to send, error.");
+        } finally {
+            btn.disabled = false;
+        }
+    });
+
+});
