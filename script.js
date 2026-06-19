2bd32db754d596f84496bd2e25e1e31a1a954a6d
Procid-GIT <Leo.Jiang@comcast.net>
2026-06-19 16:14:23 -0400

    fdhadgfgdjhsrg

--- script.js
+++ script.js
@@ -9,39 +9,39 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 async function processOCR(e) {
-        e.preventDefault(); 
-        const btn = document.getElementById("sub"); 
-        const fileInput = document.getElementById("imageUpload");
-        const output = document.getElementById("output");
+    e.preventDefault(); 
+    const btn = document.getElementById("sub"); 
+    const fileInput = document.getElementById("imageUpload");
+    const output = document.getElementById("output");
 
-        if (fileInput.files.length === 0) {
-            alert("Please upload image first, ok?");
-            return;
-        }
-
-        const formData = new FormData();
-        formData.append('aiImage', fileInput.files[0]);
-        try {
-            btn.disabled = true;
-            output.textContent = "...";
-
-            const Response = await fetch(backend_url, {
-                method: 'POST',
-                body: formData
-            })
-
-            if (!Response.ok) {
-                throw new Error(`Sorry, server returned with status code ${Response.status}`);
-            }
-
-            const result = await Response.json();
-            output.textContent = `${result.prediction}`;
-
-
-        } catch (error) {
-            console.error("Connection Error: ", error);
-            output.textContent=("Sorry, unable to send, error.");
-        } finally {
-            if (btn) btn.disabled = false;
-        }
+    if (fileInput.files.length === 0) {
+        alert("Please upload image first, ok?");
+        return;
     }
+
+    const formData = new FormData();
+    formData.append('aiImage', fileInput.files[0]);
+    try {
+        btn.disabled = true;
+        output.textContent = "...";
+
+        const Response = await fetch(backend_url, {
+            method: 'POST',
+            body: formData
+        })
+
+        if (!Response.ok) {
+            throw new Error(`Sorry, server returned with status code ${Response.status}`);
+        }
+
+        const result = await Response.json();
+        output.textContent = `${result.prediction}`;
+
+
+    } catch (error) {
+        console.error("Connection Error: ", error);
+        output.textContent=("Sorry, unable to send, error.");
+    } finally {
+        if (btn) btn.disabled = false;
+    }
+}
